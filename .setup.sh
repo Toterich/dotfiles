d9f2c0ec81c278ec3dc81be43c05d05c8053bce7
Tom Schoenfelder <mod4@gmx.de>
2020-11-22 13:32:37 +0100

    Add .setup.sh

---
+++ .setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Prerequisites
+#
+# 1. Clone this repository
+# git clone --bare <git-repo-url> $HOME/.cfg
+#
+# 2. Run this script
+
+config() {
+    /usr/bin/git --git-dir="$HOME"/.cfg/ --work-tree="$HOME" "$@"
+}
+
+# Backup local config files
+mkdir -p .config-backup && \
+config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} .config-backup/{}
+
+# Checkout config
+config checkout
+
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git "$HOME"/.vim/bundle/Vundle.vim
